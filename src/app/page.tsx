0001a2596048426fa0ff2f7ca2110210bd9b5e67
JoelVR17 <vermudo.com@gmail.com>
2024-10-05 13:21:06 -0600

    feat: welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,22 @@ import Bounded from "@/components/Bounded";
 
 export default function Home() {
   return (
-    <Bounded>
-      <h1 className="text-4xl font-bold">Welcome</h1>
+    <Bounded center={true}>
+      <div className="flex flex-col md:flex-row justify-center items-center w-full h-full mt-0 md:mt-20 gap-10">
+        <h1 className="text-5xl md:text-6xl font-bold">
+          Welcome to <br />
+          Trustless Work
+        </h1>
+        <hr className="hidden md:block bg-gray-200 w-0.5 h-96" />
+        <p className="text-xl">
+          <span className="text-primary font-bold">Escrow-as-a-service</span>{" "}
+          platform built on <strong>Soroban</strong>, <br />
+          <strong>Stellar's smart contract platform</strong>, designed to
+          provide secure,
+          <br />
+          transparent, and agile escrow solutions.
+        </p>
+      </div>
     </Bounded>
   );
 }
