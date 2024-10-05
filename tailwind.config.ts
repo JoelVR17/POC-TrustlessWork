784cc31bb7d6548c33f5a47c37935b178edb210b
JoelVR17 <vermudo.com@gmail.com>
2024-10-05 12:43:14 -0600

    feat: add primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,8 +21,8 @@ const config: Config = {
           foreground: "hsl(var(--popover-foreground))",
         },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: "#006BE4",
+          foreground: "#FFFFFF",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
