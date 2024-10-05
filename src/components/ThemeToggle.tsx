2e7453b0f5704ce2af4eafea165d34b9d2e5c940
JoelVR17 <vermudo.com@gmail.com>
2024-10-04 23:53:35 -0600

    feat: add theme toggle

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,29 @@
+import { MdOutlineLightMode, MdNightlightRound } from "react-icons/md";
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "dark" ? (
+        <MdOutlineLightMode size={25} />
+      ) : (
+        <MdNightlightRound size={25} />
+      )}
+    </button>
+  );
+}
