e1b4b8a8652a9cf47df0fd90431c3cf3c867500f
JoelVR17 <vermudo.com@gmail.com>
2024-10-05 00:03:15 -0600

    feat: add wallet functionalities, like connect, disconect and get address

---
+++ src/wallet/hooks/useWallet.hook.ts
@@ -0,0 +1,40 @@
+import {
+  allowAllModules,
+  FREIGHTER_ID,
+  ISupportedWallet,
+  StellarWalletsKit,
+  WalletNetwork,
+} from "@creit.tech/stellar-wallets-kit";
+import { useWalletStore } from "@/store/walletStore";
+
+export const useWallet = () => {
+  const { connectWalletStore, disconnectWalletStore } = useWalletStore();
+
+  const kit: StellarWalletsKit = new StellarWalletsKit({
+    modules: allowAllModules(),
+    network: WalletNetwork.TESTNET,
+    selectedWalletId: FREIGHTER_ID,
+  });
+
+  const connectWallet = async () => {
+    await kit.openModal({
+      modalTitle: "Connect to your favorite wallet",
+      onWalletSelected: async (option: ISupportedWallet) => {
+        kit.setWallet(option.id);
+
+        const { address } = await kit.getAddress();
+        connectWalletStore(address);
+      },
+    });
+  };
+
+  const disconnectWallet = async () => {
+    await kit.disconnect();
+    disconnectWalletStore();
+  };
+
+  return {
+    connectWallet,
+    disconnectWallet,
+  };
+};
---
+++ src/wallet/walletKit.ts
@@ -0,0 +1,12 @@
+import {
+  StellarWalletsKit,
+  WalletNetwork,
+  allowAllModules,
+  FREIGHTER_ID,
+} from "@creit.tech/stellar-wallets-kit";
+
+export const kit: StellarWalletsKit = new StellarWalletsKit({
+  network: WalletNetwork.TESTNET,
+  selectedWalletId: FREIGHTER_ID,
+  modules: allowAllModules(),
+});
